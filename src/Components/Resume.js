b00edbcde05d88b5998a751dcc509825f4bba9dd
abhikeshwww <137013271+abhikeshwww@users.noreply.github.com>
2023-07-11 03:29:42 +0000

    sfgsdfg

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -8,7 +8,7 @@ const Resume = () => {
         <p className="absolute mt-6 mb-6 text-4xl">Resume</p>
       </div>
 
-      <div className="flex justify-center max-sm:flex-col max-sm:justify-center  ">
+      <div className="flex justify-center max-sm:flex-col max-sm:justify-start max-sm:items-center">
         <div className="">
           <div className="flex flex-col border m-5 border-slate-400 rounded-lg p-4 w-80">
             <p className=" inline-block border w-20  px-1 text-white rounded-md text-xs  bg-green-300">
